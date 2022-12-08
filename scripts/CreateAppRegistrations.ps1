e499a155a03125078cd965b3049cf27e41cc75cb
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-08 21:34:22 +0000

    Fix path

--- scripts/CreateAppRegistrations.ps1
+++ scripts/CreateAppRegistrations.ps1
@@ -3,6 +3,6 @@ param (
     $location
 )
 
-. "./.azure-pipelines/scripts/functions/CreateAppRegistration.ps1" `
+. "scripts/functions/CreateAppRegistration.ps1" `
     -applicationName "portal-servers-integration-api-$environment" `
     -appRoles "portal-servers-integration-api-approles.json"

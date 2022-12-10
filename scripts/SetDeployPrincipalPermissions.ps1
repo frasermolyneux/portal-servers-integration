80ee4b04509d10f829d62647bdc332e835985ef1
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-10 16:48:39 +0000

    Fix script

--- scripts/SetDeployPrincipalPermissions.ps1
+++ scripts/SetDeployPrincipalPermissions.ps1
@@ -1,8 +1,6 @@
 param (
-    $environment, 
-    $keyVaultName
+    $keyVaultName,
+    $spnId
 )
 
-$principalId = az apim show --resource-group rg-platform-apim-$environment-uksouth --name apim-mx-platform-$environment-uksouth --output tsv --query 'identity.principalId'
-
-az keyvault set-policy --name $keyVaultName --spn $principalId --secret-permissions get set | Out-Null
+az keyvault set-policy --name $keyVaultName --spn $spnId --secret-permissions get set | Out-Null

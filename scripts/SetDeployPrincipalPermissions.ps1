bcd0e116e61fc8f1b05f48c757c13ad547f31cc1
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-11 11:03:57 +0000

    Fix ID

--- scripts/SetDeployPrincipalPermissions.ps1
+++ scripts/SetDeployPrincipalPermissions.ps1
@@ -6,4 +6,4 @@ param (
 $keyVaultId = az keyvault show --name $keyVaultName --output tsv --query id
 
 # https://learn.microsoft.com/en-gb/azure/role-based-access-control/built-in-roles#key-vault-secrets-officer
-az role assignment create --assignee $spnId --role 4633458b-17de-408a-b874-0445c86b69e6 --scope $keyVaultId
+az role assignment create --assignee $spnId --role b86a8fe4-44ce-4948-aee5-eccb2c155cd7 --scope $keyVaultId

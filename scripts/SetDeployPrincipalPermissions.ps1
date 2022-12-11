633b267a50acccebc280fa3f7c1a06db822b7226
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-11 10:47:51 +0000

    Switch to RBAC

--- scripts/SetDeployPrincipalPermissions.ps1
+++ scripts/SetDeployPrincipalPermissions.ps1
@@ -3,4 +3,7 @@ param (
     $spnId
 )
 
-az keyvault set-policy --name $keyVaultName --spn $spnId --secret-permissions get set | Out-Null
+$keyVaultId = az keyvault show --name $keyVaultName --output tsv --query id
+
+# https://learn.microsoft.com/en-gb/azure/role-based-access-control/built-in-roles#key-vault-secrets-officer
+az role assignment create --assignee $spnId --role 4633458b-17de-408a-b874-0445c86b69e6 --scope $keyVaultId

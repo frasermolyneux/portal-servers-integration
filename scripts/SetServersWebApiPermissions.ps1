f02f498f7fd385499c5b1f042dfd6589bea6ad55
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-01-08 15:38:13 +0000

    Adding repository API permissions grant

---
+++ scripts/SetServersWebApiPermissions.ps1
@@ -0,0 +1,18 @@
+param (
+    $environment,
+    $webAppName
+)
+
+$identity = az webapp identity show --name $webAppName --resource-group "rg-platform-webapps-$environment-uksouth" | ConvertFrom-Json
+$principalId = $identity.principalId
+
+Write-Host "Web App '$webAppName' in resource group 'rg-platform-webapps-$environment-uksouth' has principal id '$principalId'"
+
+. "scripts/functions/GrantRepositoryApiPermissionsToApp.ps1" -principalId $principalId -environment $environment
+
+$identityStaging = az webapp identity show --name $webAppName --resource-group "rg-platform-webapps-$environment-uksouth" --slot 'staging' | ConvertFrom-Json
+$principalIdStaging = $identityStaging.principalId
+
+Write-Host "Web App Slot '$webAppName/staging' in resource group 'rg-platform-webapps-$environment-uksouth' has principal id '$principalIdStaging'"
+
+. "scripts/functions/GrantRepositoryApiPermissionsToApp.ps1" -principalId $principalIdStaging -environment $environment

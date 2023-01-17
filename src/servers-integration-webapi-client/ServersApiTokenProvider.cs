9bce674be9fc42378dbb7da6605672163d0f78d6
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-01-17 19:28:05 +0000

    Again, should be DefaultAzureCredential

--- src/servers-integration-webapi-client/ServersApiTokenProvider.cs
+++ src/servers-integration-webapi-client/ServersApiTokenProvider.cs
@@ -33,7 +33,7 @@ public class ServersApiTokenProvider : IServersApiTokenProvider
                 return accessToken.Token;
         }
 
-        var tokenCredential = new ChainedTokenCredential(new ManagedIdentityCredential(), new AzureCliCredential(), new EnvironmentCredential());
+        var tokenCredential = new DefaultAzureCredential();
 
         try
         {

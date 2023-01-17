c01b0bf2eab29da36dc0e4606e11a6a64909eeb8
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-01-17 18:12:15 +0000

    requestedAccessTokenVersion to 1

--- scripts/functions/CreateAppRegistration.ps1
+++ scripts/functions/CreateAppRegistration.ps1
@@ -7,7 +7,7 @@ az ad app create --display-name "$applicationName" --identifier-uris "api://$app
 $applicationId = (az ad app list --filter "displayName eq '$applicationName'" --query '[].appId') | ConvertFrom-Json
 
 az ad app update --id "$applicationId" --sign-in-audience 'AzureADMyOrg' --enable-id-token-issuance true --enable-access-token-issuance false | Out-Null
-az rest --method PATCH --uri "https://graph.microsoft.com/v1.0/applications/$applicationId" --headers 'Content-Type=application/json' --body '{\""api\"":{\""requestedAccessTokenVersion\"":2}}'
+az rest --method PATCH --uri "https://graph.microsoft.com/v1.0/applications/$applicationId" --headers 'Content-Type=application/json' --body '{\""api\"":{\""requestedAccessTokenVersion\"":1}}'
 
 $applicationServicePrincipal = az ad sp show --id "$applicationId"
 if ($null -eq $applicationServicePrincipal) {

17ca971641ae7467e9bbd3d5400b4313b25d4d11
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-02-03 16:49:24 +0000

    Update packages and implementation

--- src/servers-integration-webapi-client/ServersApiClientOptions.cs
+++ src/servers-integration-webapi-client/ServersApiClientOptions.cs
@@ -4,14 +4,6 @@ namespace XtremeIdiots.Portal.ServersApiClient
 {
     public class ServersApiClientOptions : ApiClientOptions
     {
-        public ServersApiClientOptions(string baseUrl, string apiKey, string apiAudience) : base(baseUrl, apiKey, apiAudience)
-        {
 
-        }
-
-        public ServersApiClientOptions(string baseUrl, string apiKey, string apiAudience, string apiPathPrefix) : base(baseUrl, apiKey, apiAudience, apiPathPrefix)
-        {
-
-        }
     }
 }
